c0ebf442a63e173de068bcf512873bc075b08a95
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-08-17 04:24:11 +0600

    shadcn for svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import './layout.css';
-
+	import { ModeWatcher } from "mode-watcher";
 	let { children } = $props();
 </script>
 
 <div class="app">
-	
+	<ModeWatcher />
 	<main>{@render children()}</main>
 </div>
 
